667111e52b9a84bab441660833ed4997fa4c5b8c
Angelica Cristobal <AngelCristobal@Angelicas-MacBook-Pro-2.local>
2016-10-24 00:39:55 +0800

    movie rating

--- story.rb
+++ story.rb
@@ -2,16 +2,23 @@
 
 #write a function that accepts two parameters as strings: movie title and movie rating (out of ten)
   #the function, when called, should print the story title and the rating out of ten
-
+def display_story(title, rating)
+	puts("The movie, #{title}, has a rating of #{rating}.")
+end
 
 #show the user a message that says "Welcome to Movie Rating"
+puts ("Welcome to Movie Rating")
 
 #show the user a message asking them for the title of a movie
+puts ("What's the title of the movie?")
 
 #store the input in a variable
+user_title = gets.strip
 
 #show the user a message asking for their rating of the movie out of ten
+puts ("What's your rating for #{user_title}, out of ten?")
 
 # store the input in a variable
+user_rating = gets.strip
 
-display_story(user_title, user_rating)
+puts(display_story(user_title, user_rating))
